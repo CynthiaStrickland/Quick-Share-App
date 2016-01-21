6da9f6a547ba6a3a0fee609fac137be59a380bfd
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-01-20 16:40:53 -0800

    Initial Commit

---
+++ QuickShareUdemy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuickShareUdemy
+//
+//  Created by Cynthia Whitlatch on 1/20/16.
+//  Copyright © 2016 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
